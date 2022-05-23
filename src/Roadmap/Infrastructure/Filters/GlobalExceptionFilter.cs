a59d9259408dc5359642ff96eddf5fd88254fdf4
Igor Likhachev <Fdiii@mail.ru>
2022-05-24 01:37:32 +0300

    feat: add serilog (#4)

--- src/Roadmap/Infrastructure/Filters/GlobalExceptionFilter.cs
+++ src/Roadmap/Infrastructure/Filters/GlobalExceptionFilter.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Serilog;
 
 namespace Tiove.Roadmap.Infrastructure.Filters;
 
@@ -8,13 +9,17 @@ public class GlobalExceptionFilter : ExceptionFilterAttribute
     public override void OnException(ExceptionContext context)
     {
         string exceptionType = context.Exception.GetType().FullName ?? string.Empty;
+        string exceptionMessage = context.Exception.Message;
         string stackTrace = context.Exception.StackTrace ?? string.Empty;
 
         var resultObject = new
         {
             ExceptionType = exceptionType,
+            ExceptionMessage = exceptionMessage,
             StackTrace = stackTrace
         };
+
+        Log.Logger.Warning("Exception was thrown {@resultObject}", resultObject);
 
         var jsonResult = new JsonResult(resultObject)
         {
