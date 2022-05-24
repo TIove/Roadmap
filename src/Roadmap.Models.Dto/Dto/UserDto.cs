ee97230cea88a2895f6e1dfc00b710f6028401e7
Igor <fdiii@mail.ru>
2022-05-24 20:13:56 +0300

    fix: add not required field

--- src/Roadmap.Models.Dto/Dto/UserDto.cs
+++ src/Roadmap.Models.Dto/Dto/UserDto.cs
@@ -4,7 +4,7 @@ public class UserDto
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string MiddleName { get; set; }
+    public string? MiddleName { get; set; }
     public int Status { get; set; }
     public bool IsAdmin { get; set; }
     public bool IsActive { get; set; }
